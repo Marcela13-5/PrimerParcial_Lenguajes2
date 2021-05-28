418c4d33a08cd7441e2b6ca5575353965634f80a
Marcela <mdiazosorio5@gmail.com>
2021-05-28 00:01:57 -0500

    Initial commit

--- src/Util/Garaje.java
+++ src/Util/Garaje.java
@@ -2,6 +2,7 @@ package Util;
 import Model.Color;
 import Model.Vehiculo;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -20,9 +21,21 @@ public class Garaje {
         setVehiculo.forEach(System.out::println);
     }
 
-    List<ReferenciaVehiculo> referenciaVehiculo = setVehiculo.stream()
-            .filter(vehiculo -> (Vehiculo.getColor() = Color.BLANCO) || (Vehiculo.getColor() = Color.VERDE))
-            .map(vehiculo -> new ReferenciaVehiculo(Vehiculo.getReferencia()));
+    public valorVelocidadMaxima(){
+        setVehiculo.stream()
+                .max(Comparator.comparing(vehiculo -> vehiculo.getVelocidadMaxima()));
+    }
+
+    public valorVelocidadMenor(){
+        setVehiculo.stream()
+                .min(Comparator.comparing(vehiculo -> vehiculo.getVelocidadMaxima()));
+    }
+
+    public valorReferenciaVehiculo(){
+        setVehiculo.stream()
+                .filter(vehiculo -> (Vehiculo.getColor() = Color.BLANCO) || (Vehiculo.getColor() = Color.VERDE))
+                .map(vehiculo -> new ReferenciaVehiculo(Vehiculo.getReferencia()));
+    }
 
 
 }
